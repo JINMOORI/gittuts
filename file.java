9564b11faf3971726918bf4c517934cb8e475a53
SAI KIRAN <saikiran1611@outlook.com>
2024-06-12 11:44:22 +0530

    commiting file to copy1

---
+++ file.java
@@ -0,0 +1,28 @@
+public class file {
+
+    public static void main(String[] args) {
+        file m = new file();
+        m.sayHello();
+        m.addnumbers(65,75);
+        m.addnumbers(100,450);
+        m.addnumbers(78,82);
+        int k=m.multiplynumbers(8,9);
+        
+        System.out.println(k);
+
+    }
+
+    void sayHello()
+    {
+        System.out.println("HI");
+    }
+    void addnumbers(int a,int b)
+    {
+        System.out.println(a+b);
+    }
+    int multiplynumbers(int a,int b)
+    {
+        int x = a*b;
+        return x;
+    }
+}
